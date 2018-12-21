1e9b20ab0277d73be7691e6f6ba6080a7cd1c18d
Daniel Shervheim <danielshervheim@gmail.com>
2018-12-20 22:49:11 -0600

    started on day-night cycle manager

---
+++ Assets/SKY/Scripts/DayManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DayManager : MonoBehaviour {
+
+	public Transform sun_;
+	public float sunYaw_ = 0f;
+	private float sunPitch_ = 0f;
+
+	public Material sky_;
+	public Material cloud_;
+	
+	public float dayDuration_ = 24;  // in minutes
+	public bool pauseTime_ = true;
+	public float currentTimeNormalized_ = 0f;  // should be read-only
+
+	public DayManagerPreset[] presets;
+
+	void Start () {
+		sun_.rotation = Quaternion.identity;
+	}
+	
+	void Update () {
+		if (!pauseTime_) {
+			// update the sun rotation based on the elapsed time of day
+			sunPitch_ += Time.deltaTime * 360f / (dayDuration_ * 60f);
+			sun_.rotation = Quaternion.Euler(sunPitch_, sunYaw_, 0f);
+		}
+		
+		currentTimeNormalized_ = (Vector3.Dot(-sun_.forward, Vector3.up) + 1f) / 2f;
+	}
+}
---
+++ Assets/SKY/Scripts/DayManagerPreset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class DayManagerPreset {
+
+	public String label_;
+	public Material skyMaterial_;
+	public Material cloudMaterial_;
+	public float start_;
+	public float end_;
+	public bool visible_ = false;
+
+	public DayManagerPreset(int i) {
+		label_ = "Element " + i;
+	}
+
+	public DayManagerPreset(String label) {
+		label_ = label;
+	}
+}
